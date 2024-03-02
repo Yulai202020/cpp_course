794eb426162a4c90f7bd3f14b211091198e51750
Yulai202020 <ngmyulai@gmail.com>
2024-03-02 20:16:39 +0400

    first commit

--- loops.cpp
+++ loops.cpp
@@ -2,11 +2,40 @@
 
 int main() {
 
+    // normal for
+
     int count = 3; // or how many u want
 
-    for (int i = 0 ; i < count, i++) {
+    for (int i = 0 ; i < count; i++) {
         std::cout << "Happy Birthday !!!" << "\n";
     }
+
+    // for each
+
+    int list[3] = {1,2,3};
+
+    for (int i : list) {
+        std::cout << i << "\n";
+    }
+
+    // while
+
+    std::string a;
+
+    while (a != "q") {
+        std::cout << "Enter q for exit : ";
+        std::cin >> a;
+    }
+
+    // do while
+
+    int i = 0;
+
+    do {
+        std::cout << i << "\n";
+        i++;
+    }
+    while (i < 5);
 
     return 0;
     
